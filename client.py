5a09047f672f06a55938e6eaa1d0d219829a89d1
Hashir-Code <hashirabbasi121@gmail.com>
2023-10-26 23:31:16 +0500

    implemented AES

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import AES
 
 HOST = 'localhost'
 PORT = 5000
@@ -6,6 +7,8 @@ PORT = 5000
 def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((HOST, PORT))
+    # Create a new AESCipher object with a secret key of your choice.
+    aes_cipher = AES.AESCipher('my_secret_key')
 
     while True:
         # Get input from the user
@@ -16,14 +19,23 @@ def main():
             client_socket.close()
             break
 
+
+        # Encrypt the user input
+        encrypted_user_input = aes_cipher.encrypt(user_input)
+
+
         # Send the input to the server
-        client_socket.sendall(user_input.encode())
+        client_socket.sendall(encrypted_user_input.encode('utf-8'))
 
         # Receive the response from the server
         response = client_socket.recv(1024)
 
-        # Display the response to the user
-        print('Received response from the server:', response.decode())
+        # Decrypt the response from the server
+        decrypted_response = aes_cipher.decrypt(response.decode())
+
+        print(f'Encrypted message from server: {response}')
+        # Display the decrypted response to the user
+        print('Received response from the server:', decrypted_response)
 
     # Close the client socket
     client_socket.close()
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import AES
 
 HOST = 'localhost'
 PORT = 5000
@@ -6,6 +7,9 @@ PORT = 5000
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((HOST, PORT))
+
+
+    aes_cipher = AES.AESCipher('my_secret_key')
 
     # Listen for incoming connections
     server_socket.listen(1)
@@ -21,11 +25,20 @@ def main():
         if not data:
             break
 
-        # Display the data to the user
-        print(f'Received message from client: {data.decode()}')
+        # Decrypt the received data
+        decrypted_data = aes_cipher.decrypt(data.decode())
+        
+        print(f'Encrypted Data from client: {data}')
+        
+        # Display the decrypted data to the user
+        print(f'Received message from client: {decrypted_data}')
+
+        # Encrypt the data to send back to the client
+        SendClient = 'Confirmation message being sent to client'
+        encrypted_data = aes_cipher.encrypt(SendClient)
 
         # Send the data back to the client
-        client_socket.sendall(data)
+        client_socket.sendall(encrypted_data.encode())
 
     # Close the client socket
     client_socket.close()
