0aa7878e6e6c901f51a9e78eb5d25c97f6237dde
Hashir-Code <hashirabbasi121@gmail.com>
2023-10-27 15:54:21 +0500

    handshaking and diffiehellman implemented

--- client.py
+++ client.py
@@ -1,44 +1,63 @@
 import socket
-import AES
+import hashlib
+import DiffieHellman
+from binascii import hexlify
 
 HOST = 'localhost'
 PORT = 5000
 
+
 def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((HOST, PORT))
-    # Create a new AESCipher object with a secret key of your choice.
-    aes_cipher = AES.AESCipher('my_secret_key')
 
-    while True:
-        # Get input from the user
-        user_input = input('Enter a message to send to the server (or `exit` to quit): ')
+    # Receive the challenge from the server
+    challenge = client_socket.recv(1024).decode()
 
-        # If the user types `exit`, quit both the programs
+    # Get the password from the user
+    password = input('Enter your password: ')
+
+    # Concatenate the challenge and the entered password, then hash it using SHA256
+    concatenated = challenge + password
+    hashed_password = hashlib.sha256(concatenated.encode()).hexdigest()
+
+    # Send the hashed password back to the server
+    client_socket.sendall(hashed_password.encode())
+
+    # Receive the authentication result from the server
+    auth_result = client_socket.recv(1024).decode()
+    # print("Conneci")
+
+    flag = 1
+    if auth_result == 'False':
+        flag = 0
+        print('Authentication failed. Closing connection...')
+        return
+
+    client = DiffieHellman.DiffieHellman()
+
+    serverpublickey = client_socket.recv(1024)
+    client_socket.send(client.publicKey.to_bytes(1024, byteorder='big'))
+
+    # creating shared key
+    client.genKey(int.from_bytes(serverpublickey, byteorder='big'))
+
+    private_key = hexlify(client.getKey()).decode()
+    # print(private_key)
+    return
+
+    while flag:
+        # Now, you can send as many messages as you want to the server
+        user_input = input(
+            'Enter a message to send to the server (or `exit` to quit): ')
+
         if user_input == 'exit':
+            client_socket.sendall(user_input.encode())
             client_socket.close()
             break
 
+        client_socket.sendall(user_input.encode())
 
-        # Encrypt the user input
-        encrypted_user_input = aes_cipher.encrypt(user_input)
-
-
-        # Send the input to the server
-        client_socket.sendall(encrypted_user_input.encode('utf-8'))
-
-        # Receive the response from the server
-        response = client_socket.recv(1024)
-
-        # Decrypt the response from the server
-        decrypted_response = aes_cipher.decrypt(response.decode())
-
-        print(f'Encrypted message from server: {response}')
-        # Display the decrypted response to the user
-        print('Received response from the server:', decrypted_response)
-
-    # Close the client socket
-    client_socket.close()
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,15 +1,21 @@
 import socket
-import AES
+import hashlib
+import secrets
+import DiffieHellman
+from binascii import hexlify
 
 HOST = 'localhost'
 PORT = 5000
+PASSWORD = 'mypassword123'  # Change this to your desired password
+
+
+def generate_challenge():
+    return secrets.token_hex(16)
+
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((HOST, PORT))
-
-
-    aes_cipher = AES.AESCipher('my_secret_key')
 
     # Listen for incoming connections
     server_socket.listen(1)
@@ -17,31 +23,54 @@ def main():
     # Accept an incoming connection
     client_socket, client_address = server_socket.accept()
 
+    # Generate and send a challenge to the client
+    challenge = generate_challenge()
+    client_socket.send(challenge.encode())
+
+    # Receive the hashed password from the client
+    hashed_password = client_socket.recv(1024).decode()
+
+    # Concatenate the challenge and the server's password, then hash it using SHA256
+    concatenated = challenge + PASSWORD
+    hashed_server_password = hashlib.sha256(concatenated.encode()).hexdigest()
+
+    # Compare the hash strings for authentication
+    if hashed_password == hashed_server_password:
+        client_socket.send("Authentication done".encode())
+    else:
+        client_socket.send("False".encode())
+        client_socket.close()  # Close the client connection
+        server_socket.close()  # Close the server socket
+        return  # Exit the program
+
+    serverobject = DiffieHellman.DiffieHellman()
+    # sending public key of server
+    client_socket.send(serverobject.publicKey.to_bytes(1024, byteorder='big'))
+
+    # receiving public key of client
+    clientpublickey = client_socket.recv(1024)
+
+    # creating shared key
+    serverobject.genKey(int.from_bytes(clientpublickey, byteorder='big'))
+
+    private_key = hexlify(serverobject.getKey()).decode()
+    # print(private_key)
+
+    return
+
     while True:
-        # Receive data from the client
-        data = client_socket.recv(1024)
+        data = client_socket.recv(1024).decode()
+        if data == 'exit':
+            break  # Break out of the loop to close the connection
 
-        # If the client has closed the connection, break out of the loop
-        if not data:
-            break
-
-        # Decrypt the received data
-        decrypted_data = aes_cipher.decrypt(data.decode())
-        
-        print(f'Encrypted Data from client: {data}')
-        
-        # Display the decrypted data to the user
-        print(f'Received message from client: {decrypted_data}')
-
-        # Encrypt the data to send back to the client
-        SendClient = 'Confirmation message being sent to client'
-        encrypted_data = aes_cipher.encrypt(SendClient)
-
-        # Send the data back to the client
-        client_socket.sendall(encrypted_data.encode())
+        print(f'Received message from client: {data}')
 
     # Close the client socket
     client_socket.close()
+
+    # Close the server socket (this will only happen if the server is manually terminated)
+    server_socket.close()
+
 
 if __name__ == '__main__':
     main()
